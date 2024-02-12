efdd7a5e69f6ddf66855667022435173b55dcbda
ryhyx <reihaneh.shakiba@gmail.com>
2024-02-12 22:24:01 +0330

    OOP practice (undrestanding property and field)-timespantask

---
+++ Timespan_practice/Fieds_&_properties.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    internal class Person
+    {
+        //fields
+        private int _id;
+        private string _fullname;
+        private DateTime _birthdate;
+        private TimeSpan _age; 
+
+        //properties
+        public int ID { get { return _id; } set { _id = value; } }
+        public string Fullname { get { return _fullname; } set { _fullname = value; } }
+        public DateTime Birthdate { get { return _birthdate; } set { _birthdate = value; } }
+
+        public double Age
+        {
+            get
+            {
+                TimeSpan Calcuteage =DateTime.Now - _birthdate;
+                double _age=Calcuteage.TotalDays /365.25 ;
+                return Math.Round(_age,2);
+            }
+        }
+    }
+}
---
+++ Timespan_practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyNamespace // Same unique namespace name as in the class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person()
+            {
+                ID = 1,
+                Fullname = "Rih sha",
+                Birthdate = new DateTime(2001, 07, 05),
+            };
+            Console.WriteLine(person1.Age);
+        }
+    }
+}
