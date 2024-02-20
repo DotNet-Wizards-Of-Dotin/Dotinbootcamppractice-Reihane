298cb8e6f1434010b78901904781ef72f2ffb060
ryhyx <reihaneh.shakiba@gmail.com>
2024-02-20 16:18:07 +0330

    custom TagHelpers

---
+++ MVCpractices/MVCpractices/TagHelpers/ImageViewDetails.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MVCpractices.TagHelpers
+{
+    public class ImageViewDetails : TagHelper
+    {
+        public string src { get; set; } = "";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+            output.TagMode = TagMode.SelfClosing;
+            output.Attributes.SetAttribute("src", src);
+            output.Attributes.SetAttribute("class", "Booksize");
+            output.Attributes.SetAttribute("title", "");
+        }
+    }
+}
