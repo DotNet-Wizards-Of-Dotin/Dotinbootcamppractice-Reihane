2d2a465b7c41cf2b024d805c46ec22a5b2badf95
ryhyx <reihaneh.shakiba@gmail.com>
2024-02-28 15:19:43 +0330

    polymorphism practice

---
+++ oop_practice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var shapes = new List<shape>
+            {
+                new Regtangle
+                {
+                    ID=1,
+                    lenght = 5,
+                    width=2,
+
+                },
+                new circle
+                {
+                    ID=2,
+                    radius = 2,
+
+                },
+                new Regtangle
+                {
+
+                    ID = 3,
+                    lenght = 3,
+                    width=4,
+
+                },
+
+            };
+            foreach (var item in shapes)
+            {
+                Console.WriteLine(item.CalcuteArea());
+                Console.WriteLine(item.CalculatePerimeter() +$"\n");
+                
+            }
+        }
+    }
+}
---
+++ oop_practice/shape.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_practice
+{
+    internal class shape
+    {
+        //parent
+        public int ID {  get; set; }
+        //public virtual string printID()
+        //{
+
+        //    return $"ID={ID}";
+        //}
+        public virtual string CalculatePerimeter()
+        {
+            return $"CalculatePerimeter = ";
+        }
+        public virtual string CalcuteArea()
+        {
+            return $"CalcuteArea = ";
+        }
+
+    }
+    internal class Regtangle : shape
+    {
+        public float lenght { get; set; }
+        public float width {  get; set; }
+        public override string CalculatePerimeter()
+        {
+            var CalculatePerimeter = base.CalculatePerimeter();
+            CalculatePerimeter = $"CalculatePerimeter ID:{ID} = {(lenght * width)*2}";
+            return CalculatePerimeter ;
+        }
+        public override string CalcuteArea()
+        {
+            var CalcuteArea = base.CalcuteArea();
+            CalcuteArea = $"CalculatePerimeter ID:{ID} = {lenght * width * 3.14}";
+            return CalcuteArea;
+        }
+
+    }
+    internal class circle : shape
+    {
+        public float radius { get; set; }
+        public override string CalculatePerimeter()
+        {
+            var CalculatePerimeter = base.CalculatePerimeter();
+            CalculatePerimeter = $"CalculatePerimeter ID:{ID} = {radius*3.14}";
+            return CalculatePerimeter ;
+        }
+        public override string CalcuteArea()
+        {
+            var CalcuteArea = base.CalcuteArea();
+            CalcuteArea = $"CalculatePerimeter ID:{ID} = {radius*radius * 3.14}";
+            return CalcuteArea ;
+        }
+     
+
+
+    }
+
+}
